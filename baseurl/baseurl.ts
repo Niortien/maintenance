519cc363992516827bda0c5912a111b0ab5e7dde
niortien <niortiencoulibaly2001@gmail.com>
2026-04-06 09:58:50 +0000

    base url

--- baseurl/baseurl.ts
+++ baseurl/baseurl.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://maintenance-backend-flax.vercel.app/";
+// export const BASE_URL = "https://maintenance-backend-flax.vercel.app/";
+export const BASE_URL= "https://maintenance-backend-282w.onrender.com"
